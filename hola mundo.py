f8723a1fb45580238cab23b5fca952705b225a44
cubixpoio <mariana.leonf@uanl.edu.mx>
2024-08-14 14:53:25 -0600

    Update hola mundo.py

--- hola mundo.py
+++ hola mundo.py
@@ -2,3 +2,30 @@ print("hola mundo")
 entero = int(input())
 flotante = float(input())
 caracter =  str(input())
+#if/else/ elif
+ if x>5: 
+
+
+
+
+
+
+
+
+
+#while
+   while condicionTRUE:
+     codigo
+     ControlDeCondicion(sentinel/variable centinela)
+#for
+ for variable en objtoIterable:
+    haz algo con la variable
+
+edad = int (input("ingrese su edad por favor"))
+if edad < 18:
+    print "no puedes pasar xd"
+else edad > 18:
+   print "bienvenido"
+for lista = ["hola mundo"]
+
+  
