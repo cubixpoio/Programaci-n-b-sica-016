702edc1e7727d07c656f7ccebef02efc1d720cdc
cubixpoio <mariana.leonf@uanl.edu.mx>
2024-08-22 14:57:33 -0600

    Update # tarea 3.py

--- # tarea 3.py
+++ # tarea 3.py
@@ -2,4 +2,35 @@
   #act 1
  # solicita nom y edad y imprimir m
 print("ingrse su nombre")  
-name =int(input())
+name =input()
+Print("ingrese su edad")
+edad=input()
+edad=edad true
+if
+name=name true
+if
+print("benvenido",name
+      
+
+
+#act 2
+
+
+
+
+
+
+#act 3
+#act 4
+#act 5
+#act 6
+#act 7
+#act 8
+#act 9
+#act 10
+#act 11
+#act 12
+#act 13
+#act 14
+#act 15
+
