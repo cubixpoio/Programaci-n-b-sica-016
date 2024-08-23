af65cedd1f5f8b725455e788b35efbc552f4ad15
cubixpoio <mariana.leonf@uanl.edu.mx>
2024-08-22 22:10:32 -0600

    Update # tarea 3.py

--- # tarea 3.py
+++ # tarea 3.py
@@ -3,25 +3,31 @@
  # solicita nom y edad y imprimir m
 print("ingrse su nombre")  
 name =input()
-Print("ingrese su edad")
-edad=input()
-edad=edad true
-if
-name=name true
-if
-print("benvenido",name
-      
-
-
+print("ingrese su edad")
+edad =input()
+print("benvenido",name, "tu edad es",edad) 
+    
 #act 2
+num1 = int(input("ingrese su primer numero entero: "))
+num2 = int(input ("ingrese su segundo numero entero:  "))
 
-
-
-
-
+suma = num1 + num2
+print("la suma es:" ,suma )
+resta = num1 - num2
+print("la resta es:", resta)
+multi = num1 * num2
+print("la multiplicacion es:", multi)
+div = num1 // num2
+print("su division es:", div)
 
 #act 3
+frase = input("ingrese una frase: ")
+print (frase.lower())
+print (frase.upper())
+print (frase.title())
+
 #act 4
+
 #act 5
 #act 6
 #act 7
