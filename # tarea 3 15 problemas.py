130cb4b9911dffebad5a7c814d995c2415067512
cubixpoio <mariana.leonf@uanl.edu.mx>
2024-08-23 20:09:48 -0600

    Update # tarea 3 15 problemas.py

--- # tarea 3 15 problemas.py
+++ # tarea 3 15 problemas.py
@@ -70,14 +70,72 @@ txt = input("ingrese su nombre")
 print(cadena(txt))
 
 #act 10
+calificacion1 = float(input("Escribe la primera calificación: "))
+calificacion2 = float(input("Escribe la segunda calificación: "))
+calificacion3 = float(input("Escribe la tercera calificación: "))
+
+promedio = (calificacion1 + calificacion2 + calificacion3) / 3
+
+print("El promedio de las calificaciones es:", promedio)
 
 #act 11
+peso = float(input("Ingresa tu peso en kilogramos: "))
 
+altura = float(input("Ingresa tu altura en metros: "))
+
+
+imc = peso / (altura ** 2)
+
+if imc < 18.5:
+    rango = "Bajo peso"
+elif 18.5 <= imc < 24.9:
+    rango = "Peso normal"
+elif 25 <= imc < 29.9:
+    rango = "Sobrepeso"
+else:
+    rango = "Obesidad"
+
+print("Tu índice de masa corporal (IMC) es:", round(imc, 2))
+print("Esto se encuentra en el rango de:", rango)
 #act 12
+import random
+numero_secreto = random.randint(1, 100)
+
+intento = 0
+
+while intento != numero_secreto:
+    intento = int(input("Adivina el número entre 1 y 100: "))
+    
+    if intento < numero_secreto:
+        print("El número es mayor.")
+    elif intento > numero_secreto:
+        print("El número es menor.")
+print("¡Adivinaste el número!")
 
 #act 13
+nombres = input("Escribe los nombres separados por comas: ")
+lista_nombres = nombres.split(',')
+lista_nombres.sort()
+
+print("Nombres ordenados por inicial:")
+for nombre in lista_nombres:
+    print(nombre.strip())
 
 #act 14
 
-#act 15
+numero = int(input("Ingresa un número para mostrar su tabla de multiplicar: "))
 
+print(f"Tabla de multiplicar del {numero}:")
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
+
+#act 15
+text = input("Escribe una oración: ")
+
+def count_words(txt):
+    words = txt.split()
+  
+    count = len(words)
+    return count
+
+print("Número de palabras en la oración:", count_words(text))
